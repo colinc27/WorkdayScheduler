042faa5268a10a556ee5a55143869a59a66de817
Colin Christoph <colinchristoph27@gmail.com>
2022-03-10 15:22:37 -0700

    Add save function and extraction from local storage

--- Assets/Script.js
+++ Assets/Script.js
@@ -10,6 +10,26 @@ today = mm + '/' + dd + '/' + yyyy;
 console.log(today);
 document.getElementById('date').innerHTML="Here is your schedule for today " + today
 
+setText()
+function setText()  {
+    for(var n=7;n<19;n++){
+        var setTasks=localStorage["txt"+n]
+        console.log(setTasks);
+        document.getElementById('time'+n).value=setTasks;
+    }}
+
+$(document).ready(function(){
+    $("button[id^='save']").click(function(){
+        var id2=this.id
+        var num2=id2.substr(4)
+        console.log(id2)
+        console.log(num2)
+        var tasks=document.getElementById('time'+num2).value
+        console.log(tasks)
+        localStorage.setItem("txt"+num2,tasks)
+    })
+})
+
 $(document).ready(function(){
     $("button[id^='reset']").click(function(){
         var id=this.id
